20a1e54432498c88cd6b7e1a5f5b0ee9ab9b1b67
jevtovicc <jevtovic99@yahoo.com>
2024-09-13 17:31:24 +0200

    Osicen repo

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\BookController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
